8cdc38cb87413ac4364f9968f7eaa1e0db925244
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-12-12 22:49:47 +0100

    Shorten code using unwrap_or_else and matching

--- src/main.rs
+++ src/main.rs
@@ -29,18 +29,13 @@ lazy_static! {
 
 fn read_config(path: &Path) -> Config {
     let mut config: Config = HashMap::new();
-    let file = match File::open(&path) {
-        Ok(file) => file,
-        Err(e) => panic!("Failed to open configuration file {}: {}",
-                         path.display(), e)
-    };
+    let file = File::open(&path).unwrap_or_else(|e|
+        panic!("Failed to open configuration file {}: {}", path.display(), e));
     let mut current_target: Option<String> = None;
     for line in BufReader::new(file).lines() {
-        let line = match line {
-            Ok(line) => line,
-            Err(e) => panic!("Unable to read from configuration file {}: {}",
-                             path.display(), e)
-        };
+        let line = line.unwrap_or_else(|e|
+             panic!("Unable to read from configuration file {}: {}",
+                 path.display(), e));
         if WS.is_match(&line) {
             continue;
         }
@@ -77,10 +72,8 @@ fn prepare_matcher(config: Config) -> Matcher {
     for (target, lines) in config {
         let mut rxs = Vec::new();
         for line in &lines {
-            let rx = match Regex::new(&line) {
-                Ok(rx) => rx,
-                Err(e) => panic!("Unable to parse regex {}: {}", line, e)
-            };
+            let rx = Regex::new(&line).unwrap_or_else(|e|
+                panic!("Unable to parse regex {}: {}", line, e));
             rxs.push(rx);
         }
         let rs = RegexSet::new(&lines).unwrap();
@@ -153,24 +146,16 @@ fn main()
     };
     let mut config_dir: PathBuf = home_dir.clone();
     config_dir.push(".config/dotchaff");
-    let rd = match read_dir(&config_dir) {
-        Ok(dir) => dir,
-        Err(e) => panic!("Unable to open config directory {}: {}",
-                         config_dir.display(), e)
-    };
+    let rd = read_dir(&config_dir).unwrap_or_else(|e|
+        panic!("Unable to open config directory {}: {}",
+            config_dir.display(), e));
     let mut configs: Vec<Config> = Vec::new();
     for entry in rd {
-        let entry = match entry {
-            Ok(entry) => entry,
-            Err(e) => panic!("Failed to read directory {}: {}",
-                             config_dir.display(), e)
-        };
+        let entry = entry.unwrap_or_else(|e|
+            panic!("Failed to read directory {}: {}", config_dir.display(), e));
         let path = entry.path();
-        let filetype = match entry.file_type() {
-            Ok(filetype) => filetype,
-            Err(e) => panic!("Failed to read file type of {}: {}",
-                             path.display(), e)
-        };
+        let filetype = entry.file_type().unwrap_or_else(|e|
+            panic!("Failed to read file type of {}: {}", path.display(), e));
         if !filetype.is_file() {
             continue;
         }
@@ -178,22 +163,16 @@ fn main()
     }
     let matcher = prepare_matcher(merge_configs(configs));
     for entry in WalkDir::new(&home_dir).sort_by(fn_cmp) {
-        let entry = match entry {
-            Ok(entry) => entry,
-            Err(e) => panic!("Failed to walk directory {}: {}",
-                             home_dir.display(), e)
-        };
+        let entry = entry.unwrap_or_else(|e|
+            panic!("Failed to walk directory {}: {}", home_dir.display(), e));
         if entry.file_type().is_dir() {
             continue;
         }
-        let path = match entry.path().strip_prefix(&home_dir) {
-            Ok(path) => path,
-            Err(e) => panic!("Failed to strip prefix {} from path {}: {}",
-                             &home_dir.display(), entry.path().display(), e)
-        };
-        match do_match(&path.to_string_lossy(), &matcher) {
-            Ok(_path) => (),
-            Err(hs) => println!("!! {}: {:?}", path.display(), hs)
+        let path = entry.path().strip_prefix(&home_dir).unwrap_or_else(|e|
+            panic!("Failed to strip prefix {} from path {}: {}",
+                &home_dir.display(), entry.path().display(), e));
+        if let Err(hs) = do_match(&path.to_string_lossy(), &matcher) {
+            println!("!! {}: {:?}", path.display(), hs)
         }
     }
 }
